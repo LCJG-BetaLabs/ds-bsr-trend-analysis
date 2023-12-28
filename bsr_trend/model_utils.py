e3a8f046670737f9f143cb512c94b47cf93a9c0d
cintiaching <cintiaching@lcjgroup.com>
2023-12-28 15:33:25 +0800

    tidy up model training code

---
+++ bsr_trend/model_utils.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import itertools
+import statsmodels.api as sm
+import statsmodels
+from logger import get_logger
+
+logger = get_logger()
+
+# cluster averages
+path = "/dbfs/mnt/dev/bsr_trend/clustering/clustering_result/"
+cluster_averages = np.load(os.path.join(path, "cluster_averages.npy"), allow_pickle=True)
+
+
+def get_cluster_average(cluster_num):
+    return cluster_averages[cluster_num - 1][0]
+
+
+def extract_season_component(ts, period):
+    decomposition = sm.tsa.seasonal_decompose(ts, period=period)
+    season_component = decomposition.seasonal
+    return season_component
+
+
+# Assuming a seasonal pattern repeating every 52 weeks (annual seasonality) -> period=52
+
+def choose_d(tra, d=0, alpha=0.05, seasonal=False, period=52):
+    # Augmented Dickey-Fuller test to test for stationary
+    # p-value is less than 0.05 -> reject H0, take this series stationary
+    # else, increase d until stationary
+    if seasonal is True:
+        tra = extract_season_component(tra, period=period)
+    result = sm.tsa.stattools.adfuller(tra)
+    p_value = result[1]
+    logger.info(f"p-value: {p_value}, d: {d}")
+    if p_value < alpha:
+        # If p-value is less than 0.05, reject the null hypothesis
+        # and conclude that the series is stationary
+        return d
+    else:
+        # If p-value is greater than or equal to 0.05, increase d by 1
+        d += 1
+        return choose_d(tra.diff().dropna(), d)
+
+
+def choose_p_and_q(tra, d):
+    p_values = range(1, 10)
+    q_values = range(1, 10)
+    best_aic = float('inf')
+    best_p = 0
+    best_q = 0
+
+    # Generate all combinations of p and q
+    param_grid = itertools.product(p_values, q_values)
+    logger.info(f"param_grid: {list(param_grid)}")
+    # Perform grid search
+    for param in itertools.product(p_values, q_values):
+        p, q = param
+        # try:
+        model = sm.tsa.ARIMA(tra, order=(p, d, q))
+        results = model.fit()
+        aic = results.aic
+
+        # Update best AIC and parameters if lower AIC is found
+        if aic < best_aic:
+            best_aic = aic
+            best_p = p
+            best_q = q
+        logger.info(f"Current AIC: {aic}, p: {p}, q: {q}")
+        logger.info(f"Best AIC: {best_aic}, p: {best_p}, q: {best_q}")
+        # except:
+        #     continue
+
+    return best_p, best_q
+
+
+# TODO: early stopping for grid search
+
+def choose_seasonal_p_and_q(tra, D, s=52, order=(1, 0, 1)):
+    p_values, q_values = range(1, 10), range(1, 10)
+    best_aic = float('inf')
+    best_p, best_q = 0, 0
+
+    # Generate all combinations of p and q
+    param_grid = itertools.product(p_values, q_values)
+    logger.info(f"param_grid: {list(param_grid)}")
+
+    # Perform grid search
+    for param in itertools.product(p_values, q_values):
+        p, q = param
+        # try:
+        model = statsmodels.tsa.statespace.sarimax.SARIMAX(tra, order=order, seasonal_order=(p, D, q, s), exog=None)
+        results = model.fit()
+        aic = results.aic
+
+        # Update best AIC and parameters if lower AIC is found
+        if aic < best_aic:
+            best_aic = aic
+            best_p = p
+            best_q = q
+        logger.info(f"Current AIC: {aic}, P: {p}, Q: {q}")
+        logger.info(f"Best AIC: {best_aic}, P: {best_p}, Q: {best_q}")
+        # except:
+        #     continue
+
+    return best_p, best_q
--- bsr_trend/test_temporal_clustering/train_models.py
+++ bsr_trend/test_temporal_clustering/train_models.py
@@ -4,7 +4,24 @@
 
 # COMMAND ----------
 
+import base64
+import json
+import os
+import warnings
+
+import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percentage_error
+from tqdm import tqdm
+import statsmodels.api as sm
+
+from bsr_trend.model_utils import choose_d, choose_p_and_q, choose_seasonal_p_and_q, extract_season_component
+from bsr_trend.logger import get_logger
+
+# Suppress UserWarning from statsmodels
+warnings.simplefilter("ignore")
+
+logger = get_logger()
 
 # COMMAND ----------
 
@@ -32,25 +49,6 @@ real_pred_start = "2023-11-13"
 
 # COMMAND ----------
 
-from logger import get_logger
-
-logger = get_logger()
-
-# COMMAND ----------
-
-import base64
-import json
-import os
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import statsmodels.api as sm
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percentage_error
-from tqdm import tqdm
-
-# COMMAND ----------
 
 def one_hot_encode_month(df: pd.DataFrame, date_col: str) -> pd.DataFrame:
     df = df.copy()
@@ -62,23 +60,8 @@ def one_hot_encode_month(df: pd.DataFrame, date_col: str) -> pd.DataFrame:
             month_dummies[col] = 0
     month_dummies = month_dummies[[f"month-{i}" for i in range(1, 13)]]
     df = df.drop(columns=[c for c in month_dummies.columns if c in df.columns])
-    df = pd.concat([df, month_dummies], axis=1).drop(['month'],axis=1)
+    df = pd.concat([df, month_dummies], axis=1).drop(['month'], axis=1)
     return df
-
-# COMMAND ----------
-
-# cluster averages
-path = "/dbfs/mnt/dev/bsr_trend/clustering/clustering_result/"
-cluster_averages = np.load(os.path.join(path, "cluster_averages.npy"), allow_pickle=True)
-
-# COMMAND ----------
-
-def get_cluster_average(cluster_num):
-    return cluster_averages[cluster_num - 1][0]
-
-# COMMAND ----------
-
-df
 
 # COMMAND ----------
 
@@ -94,22 +77,7 @@ exog_pred = pred_buf
 
 # COMMAND ----------
 
-# Suppress UserWarning from statsmodels
-
-import warnings
-warnings.simplefilter("ignore")
-
-# COMMAND ----------
-
-# result = df.merge(cluster_mapping, on="vpn", how="left")
-# # cluster 1
-# result[result["cluster"] == 1]
-
-# COMMAND ----------
-
 path = "/dbfs/mnt/dev/bsr_trend/clustering/clustering_result/"
-
-# cluster_centers = pd.read_csv(os.path.join(path, "cluster_centers.csv"))
 cluster_mapping = pd.read_csv(os.path.join(path, "cluster_mapping.csv"))
 
 # COMMAND ----------
@@ -118,20 +86,11 @@ cluster_num = 2
 
 # COMMAND ----------
 
-# # get one cluster center
-# temp = df.merge(cluster_centers, on="vpn", how="left")
-# # cluster center of cluster 1
-# center_vpn = temp.loc[temp["cluster"] == cluster_num, "vpn"].values[0]
-
-# COMMAND ----------
-
-# cluster_avg = get_cluster_average(cluster_num)
-
-# COMMAND ----------
 
 def random_ts_from_cluster(cluster_num):
     vpns = cluster_mapping[cluster_mapping["cluster"] == cluster_num]["vpn"].values
     return np.random.choice(vpns, 1)[0]
+
 
 # COMMAND ----------
 
@@ -139,6 +98,7 @@ len(cluster_mapping[cluster_mapping["cluster"] == cluster_num])
 
 # COMMAND ----------
 
+# get a random ts from the cluster
 random_vpn = random_ts_from_cluster(cluster_num)
 
 subdf = df[df["vpn"] == random_vpn].set_index("order_week")
@@ -157,153 +117,31 @@ buf = buf.fillna(0).astype(int)
 # train test split
 tra = buf['qty'][tr_start:tr_end].dropna()
 tes = buf['qty'][te_start:te_end].dropna()
-exog_train = buf.drop(["qty"], axis = 1)[tr_start:tr_end].dropna()
-exog_test = buf.drop(["qty"], axis = 1)[te_start:te_end].dropna()
+exog_train = buf.drop(["qty"], axis=1)[tr_start:tr_end].dropna()
+exog_test = buf.drop(["qty"], axis=1)[te_start:te_end].dropna()
+
 
 # COMMAND ----------
 
-
-def extract_season_component(ts, period):
-    decomposition = sm.tsa.seasonal_decompose(ts, period=period)
-    season_component = decomposition.seasonal
-    return season_component
-    
-
-# COMMAND ----------
-
-from statsmodels.tsa.stattools import adfuller
-
-# Assuming a seasonal pattern repeating every 52 weeks (annual seasonality) -> period=52
-
-def choose_d(tra, d=0, alpha=0.05, seasonal=False, period=52):
-    # Augmented Dickey-Fuller test to test for stationary
-    # p-value is less than 0.05 -> reject H0, take this series stationary
-    # else, increase d until stationary
-    if seasonal is True:
-        tra = extract_season_component(tra, period=period)
-    result = sm.tsa.stattools.adfuller(tra)
-    p_value = result[1]
-    logger.info(f"p-value: {p_value}, d: {d}")
-    if p_value < alpha:
-        # If p-value is less than 0.05, reject the null hypothesis
-        # and conclude that the series is stationary
-        return d
-    else:
-        # If p-value is greater than or equal to 0.05, increase d by 1
-        d += 1
-        return choose_d(tra.diff().dropna(), d)
-
-# COMMAND ----------
-
+# choose best hyperparameter
 best_d = choose_d(tra)
-best_d
-
-# COMMAND ----------
-
 best_D = choose_d(tra, seasonal=True, period=52)
-
-# COMMAND ----------
-
-import itertools
-import statsmodels.api as sm
-
-
-def choose_p_and_q(tra, d):
-    p_values = range(1, 10)
-    q_values = range(1, 10)
-    best_aic = float('inf')
-    best_p = 0
-    best_q = 0
-
-    # Generate all combinations of p and q
-    param_grid = itertools.product(p_values, q_values)
-    logger.info(f"param_grid: {list(param_grid)}")
-    # Perform grid search
-    for param in itertools.product(p_values, q_values):
-        p, q = param
-        # try:
-        model = sm.tsa.ARIMA(tra, order=(p, d, q))
-        results = model.fit()
-        aic = results.aic
-
-        # Update best AIC and parameters if lower AIC is found
-        if aic < best_aic:
-            best_aic = aic
-            best_p = p
-            best_q = q
-        logger.info(f"Current AIC: {aic}, p: {p}, q: {q}")
-        logger.info(f"Best AIC: {best_aic}, p: {best_p}, q: {best_q}")
-        # except:
-        #     continue
-
-    return best_p, best_q
-
-# COMMAND ----------
-
 best_p, best_q = choose_p_and_q(tra, best_d)
 
-# COMMAND ----------
-
-best_p, best_q
-
-# COMMAND ----------
-
-season_component = extract_season_component(tra, period=52) 
+season_component = extract_season_component(tra, period=52)
 # Assuming a seasonal pattern repeating every 52 weeks (annual seasonality)
-
-# COMMAND ----------
-
-import statsmodels
-
-# TODO: early stopping for grid search
-
-def choose_seasonal_p_and_q(tra, D, s=52, order=(1, 0, 1)):
-    p_values, q_values = range(1, 10), range(1, 10)
-    best_aic = float('inf')
-    best_p, best_q = 0, 0
-    
-    # Generate all combinations of p and q
-    param_grid = itertools.product(p_values, q_values)
-    logger.info(f"param_grid: {list(param_grid)}")
-    
-    # Perform grid search
-    for param in itertools.product(p_values, q_values):
-        p, q = param
-        # try:
-        model = statsmodels.tsa.statespace.sarimax.SARIMAX(tra, order=order, seasonal_order=(p, D, q, s), exog=None)
-        results = model.fit()
-        aic = results.aic
-
-        # Update best AIC and parameters if lower AIC is found
-        if aic < best_aic:
-            best_aic = aic
-            best_p = p
-            best_q = q
-        logger.info(f"Current AIC: {aic}, P: {p}, Q: {q}")
-        logger.info(f"Best AIC: {best_aic}, P: {best_p}, Q: {best_q}")
-        # except:
-        #     continue
-
-    return best_p, best_q
-
-# COMMAND ----------
-
 best_P, best_Q = choose_seasonal_p_and_q(tra, best_D, s=52, order=(best_p, best_d, best_q))
 
 # COMMAND ----------
 
-best_P, best_Q = 1, 2
-
-# COMMAND ----------
-
+logger.info(f"Differencing parameter: d = {best_d}")
 logger.info(f"Seasonal differencing parameter: D = {best_D}")
+logger.info(f"AR order and MA order: p = {best_p}, q = {best_q}")
+logger.info(f"Seasonal AR order and MA order: p = {best_P}, q = {best_Q}")
 
 # COMMAND ----------
 
 temp = cluster_mapping[cluster_mapping["cluster"] == cluster_num]
-
-# COMMAND ----------
-
 temp
 
 # COMMAND ----------
@@ -331,8 +169,8 @@ for vpn in tqdm(vpns, total=len(vpns)):
     # train test split
     tra = buf['qty'][tr_start:tr_end].dropna()
     tes = buf['qty'][te_start:te_end].dropna()
-    exog_train = buf.drop(["qty"], axis = 1)[tr_start:tr_end].dropna()
-    exog_test = buf.drop(["qty"], axis = 1)[te_start:te_end].dropna()
+    exog_train = buf.drop(["qty"], axis=1)[tr_start:tr_end].dropna()
+    exog_test = buf.drop(["qty"], axis=1)[te_start:te_end].dropna()
 
     # train model
     model = sm.tsa.statespace.SARIMAX(
@@ -342,7 +180,7 @@ for vpn in tqdm(vpns, total=len(vpns)):
         order=(best_p, best_d, best_q),
         seasonal_order=(best_P, best_D, best_Q, 52),
         exog=exog_train,
-        enforce_stationarity=False, 
+        enforce_stationarity=False,
         enforce_invertibility=False,
     ).fit()
     test = model.get_prediction(te_start, te_end, exog=exog_test)
@@ -350,7 +188,7 @@ for vpn in tqdm(vpns, total=len(vpns)):
 
     # metrics
     tescopy = tes.copy()
-    tescopy[tescopy==0] = 0.1
+    tescopy[tescopy == 0] = 0.1
     mse = mean_squared_error(tescopy, test_pred)
     mape = mean_absolute_percentage_error(tescopy, test_pred)
 
@@ -371,10 +209,10 @@ for vpn in tqdm(vpns, total=len(vpns)):
     with open(f"{folder}/statsmodels.version", "w") as f:
         f.write(sm.__version__)
 
-
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC # evaluation
 
 # COMMAND ----------
 
@@ -428,7 +266,7 @@ pred_sales_velocities
 
 vel_pred = pd.merge(sales_velocities, pred_sales_velocities, how="inner", on="vpn")
 vel_pred = vel_pred.drop(columns="weekly_sales")
-vel_pred["mape (%)"] = abs(vel_pred["forecast"] / (vel_pred["gt"]+0.01) - 1) * 100
+vel_pred["mape (%)"] = abs(vel_pred["forecast"] / (vel_pred["gt"] + 0.01) - 1) * 100
 vel_pred
 
 # COMMAND ----------
@@ -478,32 +316,21 @@ metrics = pd.DataFrame(metrics)
 
 # COMMAND ----------
 
-metrics
-
-# COMMAND ----------
-
 forecast = preds
 
 # COMMAND ----------
 
 coverage = pd.read_csv("/dbfs/mnt/dev/bsr_trend/week_coverage.csv")
-forecast = forecast[[c for c in forecast.columns if c >= "2023-11-13"]] # what is this date
+forecast = forecast[[c for c in forecast.columns if c >= "2023-11-13"]]  # what is this date
 forecast = forecast.apply(sum, axis=1).reset_index()
 forecast.columns = ["vpn", "forecast"]
 
-vpn_min_qty = pd.read_csv("/dbfs/mnt/dev/bsr_trend/vpn_min_qty.csv") # waht is this
+vpn_min_qty = pd.read_csv("/dbfs/mnt/dev/bsr_trend/vpn_min_qty.csv")  # waht is this
 
 # COMMAND ----------
 
-df
-
-# COMMAND ----------
-
-vel_pred
-
-# COMMAND ----------
-
-vel_pred = vel_pred[["vpn", "forecast", "mape (%)"]].rename(columns={"mape (%)": "vel_mape (%)", "forecast": "vel_forecast"})
+vel_pred = vel_pred[["vpn", "forecast", "mape (%)"]].rename(
+    columns={"mape (%)": "vel_mape (%)", "forecast": "vel_forecast"})
 
 # COMMAND ----------
 
@@ -518,7 +345,9 @@ df["vel_forecast"] = np.ceil(df["vel_forecast"]).astype(int)
 df["stock_level"] = df["stock_level"].astype(int)
 df["po_qty"] = (df["forecast"] - df["stock_level"] + df["min_qty_hk"]).clip(lower=0)
 df["vel_po_qty"] = (df["vel_forecast"] - df["stock_level"] + df["min_qty_hk"]).clip(lower=0)
-df = df[["vpn", "supplier", "category", "class", "subclass", "mape (%)", "stock_level", "min_qty_hk", "forecast", "po_qty", "vel_forecast", "vel_po_qty", "vel_mape (%)"]]
+df = df[
+    ["vpn", "supplier", "category", "class", "subclass", "mape (%)", "stock_level", "min_qty_hk", "forecast", "po_qty",
+     "vel_forecast", "vel_po_qty", "vel_mape (%)"]]
 df
 
 # COMMAND ----------
@@ -532,5 +361,3 @@ subclass_mape
 subclass_mape.to_csv(f"/dbfs/mnt/dev/bsr_trend/clustering/{cluster_num}_subclass_mape.csv", index=False)
 
 # COMMAND ----------
-
-
