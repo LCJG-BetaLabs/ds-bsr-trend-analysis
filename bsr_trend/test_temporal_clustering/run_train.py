dd24cc477f494e98f0cad340219a05244ccbff1d
cintiaching <cintiaching>
2024-02-21 06:07:48 +0000

    tidy up train

--- bsr_trend/test_temporal_clustering/run_train.py
+++ bsr_trend/test_temporal_clustering/run_train.py
@@ -1,22 +1,54 @@
 # Databricks notebook source
 import os
 import pandas as pd
-from multiprocessing.pool import ThreadPool
-
-pool = ThreadPool(5)
+import numpy as np
 
 # compare 3 clustering method
-clustering_method = ["som", "kmeans", "kmeans_breakdown"]
+clustering_method = ["som"]  # , "kmeans", "kmeans_breakdown"]
 
 for cm in clustering_method:
     path = f"/dbfs/mnt/dev/bsr_trend/clustering/{cm}/"
     mapping = pd.read_csv(os.path.join(path, "cluster_mapping.csv"))
-    pool.map(
-        lambda cluster: dbutils.notebook.run(
-            "./train",
-            timeout_seconds=0,
-            arguments={"cluster_no": cluster, "clustering_method": cm}),
-        mapping["cluster"].values,
-    )
+    try:
+        for cluster in mapping["cluster"].values:
+            dbutils.notebook.run(
+                "./train",
+                timeout_seconds=0,
+                arguments={"cluster_no": cluster, "clustering_method": cm},
+            )
+    except:
+        print(cm, cluster)
 
 # COMMAND ----------
+
+cm = "kmeans"
+path = f"/dbfs/mnt/dev/bsr_trend/clustering/{cm}/"
+mapping = pd.read_csv(os.path.join(path, "cluster_mapping.csv"))
+try:
+    for cluster in mapping["cluster"].values:
+        dbutils.notebook.run(
+            "./train",
+            timeout_seconds=0,
+            arguments={"cluster_no": cluster, "clustering_method": cm},
+        )
+except:
+    print(cm, cluster)
+
+# COMMAND ----------
+
+cm = "kmeans_breakdown"
+path = f"/dbfs/mnt/dev/bsr_trend/clustering/{cm}/"
+mapping = pd.read_csv(os.path.join(path, "cluster_mapping.csv"))
+try:
+    for cluster in mapping["cluster"].values:
+        dbutils.notebook.run(
+            "./train",
+            timeout_seconds=0,
+            arguments={"cluster_no": cluster, "clustering_method": cm},
+        )
+except:
+    print(cm, cluster)
+
+# COMMAND ----------
+
+
--- bsr_trend/test_temporal_clustering/train.py
+++ bsr_trend/test_temporal_clustering/train.py
@@ -35,6 +35,7 @@ weekly_traffic = get_weekly_traffic().toPandas()
 # for cluster 0 - cluster 0
 path = f"/dbfs/mnt/dev/bsr_trend/clustering/{clustering_method}/"
 cluster_result = pd.read_csv(os.path.join(path, f"cluster_mapping.csv"))
+cluster_result = cluster_result[cluster_result["cluster"] == int(cluster_no)]
 
 result_path = os.path.join(path, "sarimax_result", f"cluster_{cluster_no}")
 os.makedirs(result_path, exist_ok=True)
@@ -68,6 +69,7 @@ pred_buf = pred_buf.set_index("order_week")
 _tr_start = datetime.datetime.strptime(tr_start, "%Y-%m-%d")
 _tr_end = datetime.datetime.strptime(tr_end, "%Y-%m-%d")
 diff = (_tr_end - _tr_start).days // 7
+print(diff)
 
 if diff < 104:
     dbutils.notebook.exit(f"Only {diff} weeks in the training data")
@@ -75,6 +77,7 @@ if diff < 104:
 # COMMAND ----------
 
 vpns = np.unique(sales["vpn"])
+print(len(vpns))
 # fill 0 so that the length of each series is the same
 tras = []
 tess = []
@@ -103,8 +106,10 @@ for vpn in vpns:
 zero_ts_index = [i for i, t in enumerate(tess) if sum(t) == 0]
 tras = [value for i, value in enumerate(tras) if i not in zero_ts_index]
 tess = [value for i, value in enumerate(tess) if i not in zero_ts_index]
-vpns = np.delete(vpns, zero_ts_index)
 print(f"removed {vpns[zero_ts_index]}")
+removed_vpns = [v for v in vpns[zero_ts_index]]
+removed_reason = ["0 sales in testing period" for v in vpns[zero_ts_index]]
+vpns = np.delete(vpns, zero_ts_index)
 
 # COMMAND ----------
 
@@ -121,11 +126,38 @@ tes_avg.plot()
 
 # COMMAND ----------
 
-best_p, best_d, best_q, best_P, best_D, best_Q = choose_best_hyperparameter(tra_avg)
+import json
+
+file_path = os.path.join(result_path, "hyperparameters.json")
+
+if os.path.exists(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+    best_p = data['best_p']
+    best_d = data['best_d']
+    best_q = data['best_q']
+    best_P = data['best_P']
+    best_D = data['best_D']
+    best_Q = data['best_Q']
+else:
+    best_p, best_d, best_q, best_P, best_D, best_Q = choose_best_hyperparameter(tra_avg)
+    # save best hyperparameter
+
+    hp = {
+        "best_p": best_p,
+        "best_d": best_d, 
+        "best_q": best_q, 
+        "best_P": best_P, 
+        "best_D": best_D, 
+        "best_Q": best_Q, 
+    }
+    with open(file_path, "w") as file:
+        # Write the dictionary as JSON data
+        json.dump(hp, file)
 
 # COMMAND ----------
 
-def prepare_training_data(vpn, pred_buf):
+def prepare_training_data(vpn):
     subdf = sales[sales["vpn"] == vpn].set_index("order_week")
     buf = pd.merge(
         pd.DataFrame(index=date_range),
@@ -136,6 +168,10 @@ def prepare_training_data(vpn, pred_buf):
     )
     buf["order_week"] = buf.index
 
+    pred_buf = pd.DataFrame(index=pred_date_range).reset_index()
+    pred_buf.columns = ["order_week"]
+    pred_buf = pred_buf.set_index("order_week")
+
     # dynamic start date for each product
     tr_start = buf[buf.vpn.notna()]["order_week"].min().to_pydatetime()
     logger.info(f"first purchase date: {tr_start}")
@@ -143,7 +179,9 @@ def prepare_training_data(vpn, pred_buf):
         # at least 3 months of training data, skip 
         logger.info(f"item '{vpn}' is skipped")
         status = "skip"
-        return None, None, None, None, None, status
+        removed_vpns.append(vpn)
+        removed_reason.append("< 3 months of training data")
+        # return None, None, None, None, None, status
     elif tr_start < datetime.datetime(2021, 9, 1):
         tr_start = "2021-09-01"
         status = ">=2yr"
@@ -158,8 +196,6 @@ def prepare_training_data(vpn, pred_buf):
     tra = buf['qty'][tr_start:tr_end].dropna()
     tes = buf['qty'][te_start:te_end].dropna()
 
-    # exog variable
-    # weekly_traffic
     wt = weekly_traffic.set_index("week_start_date")["weekly_traffic"]
 
     # holidays
@@ -168,65 +204,72 @@ def prepare_training_data(vpn, pred_buf):
     buf['holiday_count'] = buf.groupby(pd.Grouper(key='order_week', freq='W-MON'))['order_week'].transform(
         lambda x: sum(x.map(tag_holidays)))
 
-    # sales
     buf["is_sales_period"] = buf["order_week"].apply(lambda d: sales_period(d))
 
-    buf["weekly_traffic"] = wt
-
+    buf = buf.join(wt, how="left")
     exog = buf[["holiday_count", "is_sales_period", "weekly_traffic"]]
     exog_train = exog[tr_start:tr_end].dropna()
     exog_test = exog[te_start:te_end].dropna()
-    exog_pred = pred_buf.set_index("order_week")[["holiday_count", "is_sales_period", "weekly_traffic"]].dropna()
 
-    return tra, tes, exog_train, exog_test, exog_pred
+    pred_buf["order_week"] = pred_buf.index
+    pred_buf['order_week'] = pd.to_datetime(pred_buf['order_week'])
+    pred_buf['holiday_count'] = pred_buf.groupby(pd.Grouper(key='order_week', freq='W-MON'))['order_week'].transform(
+        lambda x: sum(x.map(tag_holidays)))
+
+    pred_buf["is_sales_period"] = pred_buf["order_week"].apply(lambda d: sales_period(d))
+    pred_buf = pred_buf.join(wt, how="left")
+    exog_pred = pred_buf[["holiday_count", "is_sales_period", "weekly_traffic"]].dropna()
+    return tra, tes, exog_train, exog_test, exog_pred, pred_buf
 
 
 # COMMAND ----------
 
 # train
 for vpn in tqdm(vpns, total=len(vpns)):
-    tra, tes, exog_train, exog_test, exog_pred = prepare_training_data(vpn, pred_buf)
-    # train model
-    model = sm.tsa.statespace.SARIMAX(
-        tra,
-        order=(best_p, best_d, best_q),
-        seasonal_order=(best_P, best_D, best_Q, 52),
-        exog=exog_train,
-        enforce_stationarity=False,
-        enforce_invertibility=False,
-    ).fit()
-    test = model.get_prediction(min(tes.index), max(tes.index), exog=exog_test)
-    # test = model.get_prediction(te_start, te_end, exog=exog_test)
-    test_pred = test.predicted_mean.fillna(0)
+    try:
+        tra, tes, exog_train, exog_test, exog_pred, pred_buf = prepare_training_data(vpn)
+        # train model
+        model = sm.tsa.statespace.SARIMAX(
+            tra,
+            order=(best_p, best_d, best_q),
+            seasonal_order=(best_P, best_D, best_Q, 52),
+            exog=exog_train,
+            enforce_stationarity=False,
+            enforce_invertibility=False,
+        ).fit()
+        test = model.get_prediction(min(tes.index), max(tes.index), exog=exog_test)
+        # test = model.get_prediction(te_start, te_end, exog=exog_test)
+        test_pred = test.predicted_mean.fillna(0)
 
-    # metrics
-    tescopy = tes.copy()
-    tescopy[tescopy == 0] = 0.1
-    mse = mean_squared_error(tescopy, test_pred)
-    mape = mean_absolute_percentage_error(tescopy, test_pred)
+        # metrics
+        tescopy = tes.copy()
+        tescopy[tescopy == 0] = 0.1
+        mse = mean_squared_error(tescopy, test_pred)
+        mape = mean_absolute_percentage_error(tescopy, test_pred)
 
-    # forecast
-    prediction = model.predict(min(pred_buf.index), max(pred_buf.index), exog=exog_pred)
-    # prediction = model.predict(pred_start, pred_end, exog=exog_pred)
+        # forecast
+        prediction = model.predict(min(exog_pred.index), max(exog_pred.index), exog=exog_pred)
 
-    # save results
-    encoded_vpn = base64.b64encode(vpn.encode("utf-8")).decode()
-    folder = os.path.join(result_path, encoded_vpn)
-    os.makedirs(folder, exist_ok=True)
+        # save results
+        encoded_vpn = base64.b64encode(vpn.encode("utf-8")).decode()
+        folder = os.path.join(result_path, encoded_vpn)
+        os.makedirs(folder, exist_ok=True)
 
-    tra.to_csv(f"{folder}/dataset_train.csv")
-    tes.to_csv(f"{folder}/dataset_test.csv")
-    test_pred.to_csv(f"{folder}/dataset_prediction_on_test.csv")
-    prediction.to_csv(f"{folder}/prediction.csv")
-    exog_train.to_csv(f"{folder}/exog_train.csv")
-    exog_test.to_csv(f"{folder}/exog_test.csv")
-    exog_pred.to_csv(f"{folder}/exog_pred.csv")
+        tra.to_csv(f"{folder}/dataset_train.csv")
+        tes.to_csv(f"{folder}/dataset_test.csv")
+        test_pred.to_csv(f"{folder}/dataset_prediction_on_test.csv")
+        prediction.to_csv(f"{folder}/prediction.csv")
+        exog_train.to_csv(f"{folder}/exog_train.csv")
+        exog_test.to_csv(f"{folder}/exog_test.csv")
+        exog_pred.to_csv(f"{folder}/exog_pred.csv")
 
-    with open(f"{folder}/metrics.txt", "w") as f:
-        f.write(json.dumps({"mse": mse, "mape": mape}, indent=None))
-    sm.iolib.smpickle.save_pickle(model, f"{folder}/arima.pkl")
-    with open(f"{folder}/statsmodels.version", "w") as f:
-        f.write(sm.__version__)
+        with open(f"{folder}/metrics.txt", "w") as f:
+            f.write(json.dumps({"mse": mse, "mape": mape}, indent=None))
+        sm.iolib.smpickle.save_pickle(model, f"{folder}/arima.pkl")
+        with open(f"{folder}/statsmodels.version", "w") as f:
+            f.write(sm.__version__)
+    except:
+        print(vpn)
 
 # COMMAND ----------
 
@@ -237,38 +280,35 @@ all_tests = []
 all_test_preds = []
 mapes = []
 for vpn in tqdm(vpns, total=len(vpns)):
-    encoded_vpn = base64.b64encode(vpn.encode("utf-8")).decode()
-    folder = os.path.join(result_path, encoded_vpn)
-    tes = pd.read_csv(f"{folder}/dataset_test.csv")
-    test = pd.read_csv(f"{folder}/dataset_prediction_on_test.csv")
-    error = mean_absolute_percentage_error(tes["qty"], test["predicted_mean"])
-    test_and_pred = [vpn, sum(tes["qty"]), sum(test["predicted_mean"])]
-    test_and_preds.append(test_and_pred)
-    mapes.append(error)
-    all_tests.append(tes)
-    all_test_preds.append(test)
+    try:
+        encoded_vpn = base64.b64encode(vpn.encode("utf-8")).decode()
+        folder = os.path.join(result_path, encoded_vpn)
+        tes = pd.read_csv(f"{folder}/dataset_test.csv")
+        test = pd.read_csv(f"{folder}/dataset_prediction_on_test.csv")
+        error = mean_absolute_percentage_error(tes["qty"], test["predicted_mean"])
+        test_and_pred = [vpn, sum(tes["qty"]), sum(test["predicted_mean"])]
+        test_and_preds.append(test_and_pred)
+        mapes.append(error)
+        all_tests.append(tes)
+        all_test_preds.append(test)
+    except:
+        print(vpn)
 
 # COMMAND ----------
 
 agg_testing_error = pd.DataFrame(test_and_preds, columns=["vpn", "gt", "model_pred"])
-agg_testing_error["model_mape (%)"] = abs(agg_testing_error["model_pred"] / (agg_testing_error["gt"] + 0.01) - 1) * 100
+agg_testing_error["model_mape (%)"] = abs(agg_testing_error["model_pred"] / (agg_testing_error["gt"]) - 1) * 100
 display(agg_testing_error)
 
 # COMMAND ----------
 
-mape = mean_absolute_percentage_error(agg_testing_error["test_qty"], agg_testing_error["test_pred"])
-mape_df = pd.DataFrame([mape], columns=["mape"])
-
-# COMMAND ----------
-
+mape = mean_absolute_percentage_error(agg_testing_error["gt"], agg_testing_error["model_pred"])
+mape_df = pd.DataFrame([mape*100], columns=["mape (%)"])
 display(mape_df)
 
 # COMMAND ----------
 
 testing_error = pd.DataFrame(zip(vpns, mapes), columns=["vpn", "mapes"])
-
-# COMMAND ----------
-
 display(testing_error)
 
 # COMMAND ----------
@@ -333,4 +373,18 @@ display(vel_pred)
 
 # join agg_testing_error and vel_pred of testing period
 result = vel_pred[["vpn", "sales_vel_pred", "vel_mape (%)"]].merge(agg_testing_error, how="left", on="vpn")
+result["info"] = f"{clustering_method}_{cluster_no}"
 result.to_csv(os.path.join(result_path, "agg_result.csv"), index=False)
+display(result)
+
+# COMMAND ----------
+
+# save skipped items
+removed = pd.DataFrame(zip(removed_vpns, removed_reason), columns=["vpn", "reason_for_skipping"])
+removed.to_csv(os.path.join(result_path, "skipped_vpns.csv"), index=False)
+if len(removed):
+    display(removed)
+
+# COMMAND ----------
+
+
